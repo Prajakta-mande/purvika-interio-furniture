1aa7d2b5e87587afe4f53f0cd1d077695cc2d1b4
Prajakta-mande <ppmande29@gmail.com>
2026-01-01 18:03:58 +0530

    Initial commit

--- src/component/Commercial/TurnkeySolution.tsx
+++ src/component/Commercial/TurnkeySolution.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./turnkeySolution.css";
+import "./TurnkeySolution.css";
 import { NavLink } from "react-router-dom";
 
 const ModularOfficeFurniture: React.FC = () => {
